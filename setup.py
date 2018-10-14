ce3d5d6919ef5b8497f5843173b6a90930a2b960
himkt <himkt@klis.tsukuba.ac.jp>
2018-10-14 13:59:47 +0900

    Add GPU supoprt

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
 from distutils.core import setup
 from setuptools import find_packages
+from subprocess import Popen
+from subprocess import PIPE
+
+
+def has_gpu():
+    try:
+        p = Popen(["nvidia-smi"], stdout=PIPE)
+        stdout, stderror = p.communicate()
+        return True
+
+    except Exception:
+        return False
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+install_requires = ["numpy"]
+if has_gpu():
+    install_requires.append("tensorflow-gpu>=1.6")
+else:
+    install_requires.append("tensorflow>=1.6")
+
 
 setup(
     name="tf_metrics",
@@ -17,8 +38,4 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ),
-    install_requires=[
-        "numpy",
-        "tensorflow>=1.6"
-    ]
 )
